786cdf95cdcab44d7077a67e7674a192c8285bf4
Michael De Madet <michaeldemadet@gmail.com>
2019-03-16 08:00:39 +0100

    deploy

--- renderer/src/index.js
+++ renderer/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import Root from './containers/root/root';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Root />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
   );
 
